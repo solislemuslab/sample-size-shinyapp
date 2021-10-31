6cc4eccebbbd47003ba59a9d3803ee042ce8a1e6
Nicole Athanasiou <nicolemarieath@gmail.com>
2021-10-31 18:31:47 -0500

    update

--- app.R
+++ app.R
@@ -5,98 +5,97 @@ library(DT)
 library(shiny)
 library(tidyverse)
 library(RColorBrewer)
-library(ggfortify)
-library(survival)
 
 
 ui <- fluidPage(
-   theme = bslib::bs_theme(bootswatch = "flatly"),
+  theme = bslib::bs_theme(bootswatch = "slate"),
+  
+  tabsetPanel(
+    #sample size for equality of means
+    tabPanel("Equality of Means",
+             titlePanel("Sample Size for Equality of Means"),
+             fluidRow(
+               column(3,
+                      "Distribution 1",
+                      numericInput("mu1", "Mean 1", value = 132.85, min = 0),
+                      numericInput("s1", "Standard Deviation 1", value = 15.34, min = 0)
+               ),
+               column(3,
+                      "Distribution 2",
+                      numericInput("mu2", "Mean 2", value = 127.44, min = 0),
+                      numericInput("s2", "Standard Deviation 2", value = 18.23, min = 0)
+               ),
+               column(4,
+                      "Significance Level and Power",
+                      sliderInput("alpha", label = "alpha", value = 0.05, min = 0, max = 0.05, step = 0.001),
+                      sliderInput("power", label = "Power", value = 0.08, min = 0, max = 1, step = 0.001)
+               ),
+             ),
+             
+             #output sample size n
+             fluidRow(
+               column(10, verbatimTextOutput("n_norm"))
+             ),
+             #output normal plot
+             fluidRow(
+               column(10, plotOutput("plotnorm"))
+             )),
+    #sample size for binomial proportions
+    tabPanel("Two Binomial Distributions",
+             titlePanel("Sample Size for Comparing Two Binomial Proportions"),
+             fluidRow(
+               column(3,
+                      "Distribution 1",
+                      numericInput("p1", label = "Proportion 1", value = 0.0015, min = 0, max = 1),
+                      "Distribution 2",
+                      numericInput("p2", label = "Proportion 2", value = 0.0012, min = 0, max = 1),
+                      numericInput("k", label = "k", value = 1, min = 1)),
+               column(3,
+                      "Significance Level and Power",
+                      sliderInput("alpha_bin", label = "alpha", value = 0.05, min=0, max=0.05, step = 0.001),
+                      sliderInput("power_bin", label = "Power", value = 0.08, min = 0, max = 1, step = 0.001),
+               )),
+             #output sample size n
+             fluidRow(
+               column(8, verbatimTextOutput("n_bin"))
+             ),
+             #output bin plot
+             fluidRow(
+               column(10, plotOutput("plotbin"))
+             )),
     
-    tabsetPanel(
-        #sample size for equality of means
-        tabPanel("Equality of Means",
-                 titlePanel("Sample Size for Equality of Means"),
-                 fluidRow(
-                     column(3,
-                            "Distribution 1",
-                            numericInput("mu1", "Mean 1", value = 132.85, min = 0),
-                            numericInput("s1", "Standard Deviation 1", value = 15.34, min = 0)
-                     ),
-                     column(3,
-                            "Distribution 2",
-                            numericInput("mu2", "Mean 2", value = 127.44, min = 0),
-                            numericInput("s2", "Standard Deviation 2", value = 18.23, min = 0)
-                     ),
-                     column(4,
-                            "Significance Level and Power",
-                            sliderInput("alpha", label = "alpha", value = 0.05, min = 0.001, max = 0.05, step = 0.001),
-                            sliderInput("power", label = "Power", value = 0.8, min = 0.001, max = .999, step = 0.001)
-                     ),
-                 ),
-                 
-                 #output sample size n
-                 fluidRow(
-                     column(10, textOutput("n_norm"))
-                 ),
-                 #output normal plot
-                 fluidRow(
-                     column(10, plotOutput("plotnorm"))
-                 )),
-        #sample size for binomial proportions
-        tabPanel("Two Binomial Distributions",
-                 titlePanel("Sample Size for Comparing Two Binomial Proportions"),
-                 fluidRow(
-                     column(3,
-                            "Distribution 1",
-                            numericInput("p1", label = "Proportion 1", value = 0.0015, min = 0, max = 1),
-                            "Distribution 2",
-                            numericInput("p2", label = "Proportion 2", value = 0.0012, min = 0, max = 1),
-                            numericInput("k", label = "k", value = 1, min = 1)),
-                     column(3,
-                            "Significance Level and Power",
-                            sliderInput("alpha_bin", label = "alpha", value = 0.05, min = 0.001, max = 0.05, step = 0.001),
-                            sliderInput("power_bin", label = "Power", value = 0.8, min = 0.001, max = 0.999, step = 0.001),
-                     )),
-                 #output sample size n
-                 fluidRow(
-                     column(8, textOutput("n_bin"))
-                 ),
-                 #output bin plot
-                 fluidRow(
-                   column(10, plotOutput("plotbin"))
-        )),
-        
-        #sample size for survival curves
-        tabPanel("Survival Curves",
-                 titlePanel("Sample Size for Survival Curves"), 
-                 fluidRow(
-                     column(3, 
-                            "Group 1",
-                            numericInput("hr", "Hazard Ratio", value = 0.7, min = 0, max = 1),
-                            numericInput("k_survival", "k", value = 1, min = 0)),
-                     column(3,
-                            "Proportions",
-                            numericInput("pE", "Proportion Participants in Experimental", value = 0.3707, min = 0, max = 1),
-                            numericInput("pC", "Proportion Participants in Control", value = 0.4890, min = 0, max = 1)),
-                     column(4,
-                            "Significance Level and Power",
-                            sliderInput("alpha_survival", label = "alpha", value = 0.05, min = 0.001, max = 0.05, step = 0.001),
-                            sliderInput("power_survival", label = "Power", value = 0.8, min = 0.001, max = 0.999, step = 0.001)
-                            
-                     )
-                 ),
-                 #output sample size n
-                 fluidRow(
-                     column(8, textOutput("n_survival"))
-                 )
-        )
+    #sample size for survival curves
+    tabPanel("Survival Curves",
+             titlePanel("Sample Size for Survival Curves"), 
+             fluidRow(
+               column(3, 
+                      "Group 1",
+                      numericInput("hr", "Hazard Ratio", value = 0.7, min = 0, max = 1),
+                      numericInput("k_survival", "k", value = 1, min = 0)),
+               column(3,
+                      numericInput("pE", "Proportion Participants in Experimental", value = 0.3707, min = 0, max = 1),
+                      numericInput("pC", "Proportion Participants in Control", value = 0.4890, min = 0, max = 1)),
+               column(4,
+                      "Significance Level and Power",
+                      sliderInput("alpha_survival", label = "alpha", value = 0.05, min = 0, max = 0.05, step = 0.001),
+                      sliderInput("power_survival", label = "Power", value = 0.08, min = 0, max = 1, step = 0.001)
+                      
+               )
+             ),
+             #output sample size n
+             fluidRow(
+               column(8, verbatimTextOutput("n_survival"))
+             )
     )
+  )
 )
 server <- function(input, output, session) {
   
-  #normal n output
+  #normal  n
   output$n_norm = renderText({
-    n_norm = round(n_norm_reactive(), digits = 3)
+    zalpha = qnorm(1-input$alpha/2,0,1)
+    zbeta = qnorm(input$power,0,1)
+    n_norm = round(((input$s1*input$s1 + input$s2*input$s2)*(zalpha+zbeta)^2)/(input$mu2-input$mu1)^2, digits = 3)
     paste("Sample Size: ", n_norm)
   })
   
@@ -109,60 +108,65 @@ server <- function(input, output, session) {
   })
   
   #plot reactive normal n
-    output$plotnorm = renderPlot({
-        rand = data.frame(1:(n_norm_reactive()), rnorm(n_norm_reactive(), input$mu1, input$s1), rnorm(n_norm_reactive(), input$mu2, input$s2))
-        colnames(rand) = c("subjects", "values", "values2")
-        ggplot(rand, geom = 'blank') +   
-          geom_line(aes(x = values, y = ..density.., colour = 'Population 1', col = "#1B9E77"), stat = 'density') +  
-          geom_line(aes(x = values2, y = ..density.., colour = 'Population 2', col = "#D95F02"), stat = 'density') +
-          stat_function(fun = dnorm) +                       
-          geom_histogram(aes(x=values, y = ..density..), alpha = 0.4,fill = "#1B9E77", col = "#1B9E77") +    
-          geom_histogram(aes(x= values2, y = ..density..), alpha = 0.4,fill = "#D95F02", col = "#D95F02") +
-          xlab("Values") + 
-          ylab("Frequency") +
-          theme(panel.background = element_rect(fill = "transparent"),
-                plot.background = element_rect(fill = "transparent", color = NA))
-        
+  output$plotnorm = renderPlot({
+    rand = data.frame(1:(n_norm_reactive()), rnorm(n_norm_reactive(), input$mu1, input$s1), rnorm(n_norm_reactive(), input$mu2, input$s2))
+    colnames(rand) = c("subjects", "values", "values2")
+    ggplot(rand) +
+      geom_histogram(aes(x=values), fill = "#1B9E77", colour = "#1B9E77", alpha = 0.5)+
+      geom_histogram(aes(x=values2), fill = "#D95F02", colour = "#D95F02", alpha = 0.5)+
+      xlab("Values") + 
+      ylab("Frequency") +
+      theme(panel.background = element_rect(fill = "transparent"),
+            plot.background = element_rect(fill = "transparent", color = NA))
+    
   })
+  
+  #binomial n
+  output$n_bin = renderText({
+    q1 = 1-input$p1
+    q2 = 1-input$p2
+    zalpha = qnorm(1-input$alpha_bin/2,0,1)
+    zbeta = qnorm(input$power_bin,0,1)
+    p = (input$p1+input$k*input$p2)/(1+input$k)
+    q = 1-p
+    num = sqrt(p*q*(1+1/input$k))*zalpha + sqrt(input$p1*q1+input$p2*q2/input$k)*zbeta
+    n_bin = round(num^2 / (input$p1-input$p2)^2, digits = 3)
+    paste("Sample Size: ", n_bin)
+  })
+  
+  #reactive binomial n
+  n_bin_reactive = reactive({
+    q1 = 1-input$p1
+    q2 = 1-input$p2
+    zalpha = qnorm(1-input$alpha_bin/2,0,1)
+    zbeta = qnorm(input$power_bin,0,1)
+    p = (input$p1+input$k*input$p2)/(1+input$k)
+    q = 1-p
+    num = sqrt(p*q*(1+1/input$k))*zalpha + sqrt(input$p1*q1+input$p2*q2/input$k)*zbeta
+    n_bin_reactive = num^2 / (input$p1-input$p2)^2
+    n_bin_reactive
+  })
+  
+  #plot reactive normal n
+  output$plotbin = renderPlot({
+    rand = data.frame(1:(n_bin_reactive()), rbinom(1:(n_bin_reactive()), n_bin_reactive(), input$p1))
+    colnames(rand) = c("subjects", "values3")
+    ggplot(rand) +
+      geom_histogram(aes(x=values3), fill = "#1B9E77", colour = "#1B9E77", alpha = 0.5)+
+      #geom_histogram(aes(x=values2), fill = "#D95F02", colour = "#D95F02", alpha = 0.5)+
+      xlab("Values") + 
+      ylab("Frequency") +
+      theme(panel.background = element_rect(fill = "transparent"),
+            plot.background = element_rect(fill = "transparent", color = NA))
     
-    #binomial n output
-    output$n_bin = renderText({
-        n_bin = round(n_bin_reactive(), digits = 3)
-        paste("Sample Size: ", n_bin)
-    })
-    
-    #reactive binomial n
-    n_bin_reactive = reactive({
-      q1 = 1-input$p1
-      q2 = 1-input$p2
-      zalpha = qnorm(1-input$alpha_bin/2,0,1)
-      zbeta = qnorm(input$power_bin,0,1)
-      p = (input$p1+input$k*input$p2)/(1+input$k)
-      q = 1-p
-      num = sqrt(p*q*(1+1/input$k))*zalpha + sqrt(input$p1*q1+input$p2*q2/input$k)*zbeta
-      n_bin_reactive = num^2 / (input$p1-input$p2)^2
-      n_bin_reactive
-    })
-    
-    #plot reactive binomial n
-    output$plotbin = renderPlot({
-      data_bin = data.frame(x = 1:(n_bin_reactive()), y = dbinom(1:(n_bin_reactive()), n_bin_reactive(), input$p1))
-      ggplot(data_bin, aes(x=x, y=y)) +
-        geom_bar(stat="identity", fill = "#1B9E77", col = "#1B9E77")+
-        #geom_histogram(aes(x=values2), fill = "#D95F02", colour = "#D95F02", alpha = 0.5)+
-        xlab("Values") + 
-        ylab("Probability") +
-        theme(panel.background = element_rect(fill = "transparent"),
-              plot.background = element_rect(fill = "transparent", color = NA))
-      
-    })
-    
-    output$n_survival = renderText({
-        zalpha = qnorm(1-input$alpha/2,0,1)
-        zbeta = qnorm(input$power,0,1)
-        m = (1/input$k)*((input$k*input$hr+1)/(input$hr-1))^2*(zalpha+zbeta)^2
-        n_survival = m/(input$k*input$pE + input$pC)
-    })
-    
+  })
+  
+  output$n_survival = renderText({
+    zalpha = qnorm(1-input$alpha/2,0,1)
+    zbeta = qnorm(input$power,0,1)
+    m = (1/input$k)*((input$k*input$hr+1)/(input$hr-1))^2*(zalpha+zbeta)^2
+    n_survival = m/(input$k*input$pE + input$pC)
+  })
+  
 }
 shinyApp(ui = ui, server = server)
